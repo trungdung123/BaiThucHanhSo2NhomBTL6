ff8576ea116775069c6409601ede7770462fecda
trungdung <phamrungdungasao@gmail.com>
2022-05-07 14:47:40 +0700

    update project

---
+++ src/main/java/com/demo/BaiThucHanhSo2NhomBtl6Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiThucHanhSo2NhomBtl6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaiThucHanhSo2NhomBtl6Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/demo/controllers/ProductController.java
@@ -0,0 +1,80 @@
+package com.demo.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.demo.models.Product;
+import com.demo.service.ProductService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+	
+	@Autowired
+	private ProductService productService;
+
+	@GetMapping("")
+	public String listProducts (Model model) {
+		model.addAttribute("products", productService.getAllProduct());
+		return "/product/products";
+	}
+	
+	@GetMapping("/add-new-product")
+	public String addNewProductGet (Model model) {
+		Product product = new Product();
+		model.addAttribute("product", product);
+		return "/product/add-new-product";
+	}
+	
+	@PostMapping("/add-new-product")
+	public String addNewProductPost (Model model
+			, @ModelAttribute(name = "product") Product product) {
+		productService.createProduct(product);
+		return "redirect:/products";
+	}
+	
+	@GetMapping("/edit-product/{id}")
+	public String editProductGet (Model model, 
+			@PathVariable(name = "id") Long id) {
+		Product product = productService.getProductById(id);
+		model.addAttribute("product", product);
+		return "/product/product";
+	}
+	
+	@PostMapping("/edit-product/{id}")
+	public String editProductPost (Model model, 
+			@PathVariable(name = "id") Long id
+			, @ModelAttribute(name = "product") Product product) {
+		productService.editProduct(id, product);
+		return "redirect:/products";
+	}
+	
+	@GetMapping("/delete-product/{id}")
+	public String deleteProductGet (Model model
+			, @PathVariable(name = "id") Long id) {
+		
+		Product product = productService.getProductById(id);
+		model.addAttribute("product", product);
+		return "/product/delete-product";
+	}
+	
+	@PostMapping("/delete-product/{id}")
+	public String deleteProductPost (HttpServletRequest request , Model model
+			, @PathVariable(name = "id") Long id) {
+		
+		String choose = request.getParameter("choose");
+		if (choose.equals("yes")){
+			productService.deleteProduct(productService.getProductById(id));
+		}
+		return "redirect:/products";
+	}
+	
+}
